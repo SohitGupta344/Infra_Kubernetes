4b40b2b7bb4cf854ff79ae8d4d3f18ba993739b4
SohitGupta344 <sohitg89@gmail.com>
2026-08-09 02:50:34 +0530

    new-updated

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,26 +1,16 @@
 terraform {
-
   required_version = ">= 1.10.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.38"
+      version = "~> 4.38"
     }
-  }
-  
-  backend "azurerm" {
-    resource_group_name  = "sohit-rg"
-    storage_account_name = "sohittfstate001"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-    tenant_id            = "a7dd8b02-fc7e-452c-9b2c-c53db150afd2"
-    subscription_id      = "ff8dbcde-c654-4f43-be29-1fa4ab0bad46"
-
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "ff8dbcde-c654-4f43-be29-1fa4ab0bad46"
+  use_oidc = true
 }
+
